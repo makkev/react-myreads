4f185e1fbb5c50762e5bb38de6e2aa7b65c08086
K Mak <kumarajiva08@gmail.com>
2018-03-15 07:52:09 +1100

    feat: List all books loaded from API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import ListBooks from './ListBooks';
+import * as BooksAPI from './BooksAPI';
 
 class BooksApp extends Component {
+
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books});
+    });
+  }
+
   render() {
     return (
       <div>
-        Hello World
+        <ListBooks books={this.state.books} />
       </div>
     );
   }
 }
 
-export default BooksApp
+export default BooksApp;
