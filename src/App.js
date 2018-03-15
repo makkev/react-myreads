778a741008bc3a254584cf7fbeb6f9c8df3dced9
Kevin Mak <kevinmak05@gmail.com>
2018-03-15 15:50:16 +1100

    feat: break up components, add search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
 import ListBooks from './ListBooks';
+import Search from './Search'
 import * as BooksAPI from './BooksAPI';
+import './App.css'
 
 class BooksApp extends Component {
 
   state = {
-    books: []
+    books: [],
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
   };
 
+  setShowSearchPage = (showPage) => {
+    this.setState({ showSearchPage: showPage })
+  }
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
-      this.setState({books});
+      this.setState({ books });
     });
   }
 
   render() {
     return (
-      <div>
-        <ListBooks books={this.state.books} />
+      <div className="app">
+        {this.state.showSearchPage ? (
+          <Search setShowSearchPage={this.setShowSearchPage} />
+        ) : (
+          <ListBooks books={this.state.books} setShowSearchPage={this.setShowSearchPage} />
+        )}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import App_orig from './App_orig';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App_orig />, document.getElementById('root'));
