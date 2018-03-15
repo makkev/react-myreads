a0256c478eea75ec142f340ae6290a7bcfbafbbc
K Mak <kumarajiva08@gmail.com>
2018-03-15 21:49:46 +1100

    feat: sort books by bookshelf

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Bookshelf from './Bookshelf';
 
 class ListBooks extends Component {
 
@@ -8,37 +9,24 @@ class ListBooks extends Component {
       <div className="list-books">
         <ListBooksHeader />
         <div className="list-books-content">
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {this.props.books.map((book) => (
-                  <li key={book.title}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{
-                          width: 128,
-                          height: 193,
-                          backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                        }}></div>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="none" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors}</div>
-                    </div>
-                  </li>
-                ))}
-              </ol>
-            </div>
-          </div>
+          <Bookshelf 
+            books={this.props.books.filter((book) => (
+              book.shelf === 'currentlyReading'
+            ))} 
+            bookshelfTitle='Currently Reading'
+          />
+          <Bookshelf 
+            books={this.props.books.filter((book) => (
+              book.shelf === 'wantToRead'
+            ))} 
+            bookshelfTitle='Want To Read'
+          />
+          <Bookshelf 
+            books={this.props.books.filter((book) => (
+              book.shelf === 'read'
+            ))} 
+            bookshelfTitle='Read'
+          />
         </div>
         <div className="open-search">
           <a onClick={() => this.props.setShowSearchPage(true) }>Add a book</a>
